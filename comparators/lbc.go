deceb1ebed275044ffb86abc42866ec18e0d9025
JC Lebreton <jclebreton@gmail.com>
2018-02-13 13:25:57 +0100

    Remove useless lbc methods

--- comparators/lbc.go
+++ comparators/lbc.go
@@ -2,57 +2,15 @@ package comparators
 
 import (
 	"crypto/sha1"
-	"errors"
 	"fmt"
-	"math/rand"
 	"strings"
-	"time"
 )
-
-var r *rand.Rand // Rand for this package.
-
-func init() {
-	r = rand.New(rand.NewSource(time.Now().UnixNano()))
-}
-
-func randomString(strlen int) string {
-	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-	result := ""
-	for i := 0; i < strlen; i++ {
-		index := r.Intn(len(chars))
-		result += chars[index : index+1]
-	}
-	return result
-}
-
-func hashString(s string) string {
-	h := sha1.New()
-	h.Write([]byte(s))
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
 
 // LBCPassword is the struct to store LBC authentication fields
 type LBCPassword struct {
 	Hash  string
 	Salt  string
 	Plain string
-}
-
-// SetRandomSalt generates a random salt and reset other fields
-func (p *LBCPassword) SetRandomSalt() {
-	p.Salt = randomString(16)
-	p.Plain = ""
-	p.Hash = ""
-}
-
-// SetPlainPassword initializes current struct from a plain password
-func (p *LBCPassword) SetPlainPassword(password string) error {
-	if len(p.Salt) != 16 {
-		return errors.New("salt error")
-	}
-	p.Hash = p.Salt + hashString(p.Salt+password)
-	p.Plain = password
-	return nil
 }
 
 // GetHash returns the current LBC hash
@@ -69,6 +27,8 @@ func (p *LBCPassword) SetHash(hash string) {
 
 // Compare will matches plain password with LBC hash
 func (p *LBCPassword) Compare(plain string) bool {
-	hash := p.Salt + hashString(p.Salt+plain)
+	h := sha1.New()
+	h.Write([]byte(p.Salt + plain))
+	hash := p.Salt + fmt.Sprintf("%x", h.Sum(nil))
 	return strings.Compare(hash, p.Hash) == 0
 }
--- comparators/lbc_test.go
+++ comparators/lbc_test.go
@@ -4,42 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func Test_hashString_success(t *testing.T) {
-	require.Equal(t, "153fa238cec90e5a24b85a79109f91ebe68ca481", hashString("qwerty1234"))
-}
-
-func Test_randomString_success(t *testing.T) {
-	salt := randomString(128)
-	require.Equal(t, 128, len(salt))
-}
-
-func TestSetRandomSalt_success(t *testing.T) {
-	p := &LBCPassword{}
-	p.Plain = "a"
-	p.Hash = "b"
-	p.SetRandomSalt()
-	assert.Equal(t, 16, len(p.Salt))
-	assert.Equal(t, "", p.Plain)
-	assert.Equal(t, "", p.Hash)
-}
-
-func TestSetPassword_success(t *testing.T) {
-	p := &LBCPassword{}
-	p.Salt = "d2rsph111lxo3twk"
-	err := p.SetPlainPassword("qwerty1234")
-	require.NoError(t, err)
-	assert.Equal(t, p.Salt+"a829f192f7fd38700cacdc5c645596ce3e9d09b1", p.Hash)
-	assert.Equal(t, "qwerty1234", p.Plain)
-}
-
-func TestSetPassword_without_salt(t *testing.T) {
-	p := &LBCPassword{}
-	err := p.SetPlainPassword("qwerty1234")
-	require.Error(t, err)
-}
 
 func TestSetHash_success(t *testing.T) {
 	p := &LBCPassword{}
