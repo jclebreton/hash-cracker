bb0a88c65260d1f04e334690c6c450304d25ec93
JC Lebreton <jclebreton@gmail.com>
2018-02-17 19:12:45 +0100

    Split progress bars

--- cmd/run.go
+++ cmd/run.go
@@ -7,6 +7,8 @@ import (
 
 	"strings"
 
+	"fmt"
+
 	"github.com/jclebreton/hash-cracker/dictionaries"
 	"github.com/jclebreton/hash-cracker/hashers"
 	"github.com/pkg/errors"
@@ -20,20 +22,12 @@ func Run(h dictionaries.Provider, d dictionaries.Provider, hasher hashers.Hasher
 	errChan := make(chan error)
 	resultChan := make(chan map[int]Hash, 10000)
 
-	//Progression bars
-	pb1 := pb.New(0).SetUnits(pb.U_NO).Prefix("Dictionary")
-	pb2 := pb.New(0).SetUnits(pb.U_NO).Prefix("    Hashes")
-	pb3 := pb.New(0).SetUnits(pb.U_NO).Prefix("   Cracked")
-	pool, err := pb.StartPool(pb1, pb2, pb3)
-	pb1.ShowPercent = true
-	pb2.ShowPercent = true
-	pb3.ShowPercent = true
-	if err != nil {
-		logrus.WithError(err).Fatal("progress bar error")
-	}
-
 	// Read dictionary
-	dictionary, err := DictionaryReader(pb1, d)
+	pbDictionary := pb.New(0).SetUnits(pb.U_NO).Prefix("Dictionary")
+	pbDictionary.Start()
+	pbDictionary.ShowPercent = true
+	pbDictionary.ShowTimeLeft = true
+	dictionary, err := DictionaryReader(pbDictionary, d)
 	if err != nil {
 		logrus.WithError(err).Error("dictionary provider error")
 	}
@@ -46,10 +40,28 @@ func Run(h dictionaries.Provider, d dictionaries.Provider, hasher hashers.Hasher
 	// Init workers
 	hashesChans := make(map[int]chan Hash, 10000)
 	resetChans := make(map[int]chan Hash)
+	var progressBar *pb.ProgressBar
+	var progressBars []*pb.ProgressBar
+
 	for i := 1; i <= nbWorkers; i++ {
 		wg.Add(1)
-		resetChans[i], hashesChans[i] = worker(i, &wg, errChan, dictionaries[i-1], resultChan)
+		resetChans[i], hashesChans[i], progressBar = worker(i, &wg, errChan, dictionaries[i-1], resultChan)
+		progressBars = append(progressBars, progressBar)
 		logrus.WithField("id", i).Debug("worker started")
+	}
+
+	pbHashes := pb.New(0).SetUnits(pb.U_NO).Prefix("Hashes")
+	progressBars = append(progressBars, pbHashes)
+	pbCracked := pb.New(0).SetUnits(pb.U_NO).Prefix("Cracked")
+	progressBars = append(progressBars, pbCracked)
+	pool, err := pb.StartPool(progressBars...)
+	pbHashes.ShowPercent = true
+	pbHashes.ShowTimeLeft = true
+	pbCracked.ShowPercent = true
+	pbCracked.ShowTimeLeft = false
+
+	if err != nil {
+		logrus.WithError(err).Fatal("progress bar error")
 	}
 
 	// Provider error
@@ -75,7 +87,7 @@ func Run(h dictionaries.Provider, d dictionaries.Provider, hasher hashers.Hasher
 	}()
 
 	// Read hashes
-	go HashesReader(pb2, pb3, h, errChan, hashesChans, hasher)
+	go HashesReader(pbHashes, pbCracked, h, errChan, hashesChans, hasher)
 
 	// Success
 	go func() {
@@ -88,7 +100,7 @@ func Run(h dictionaries.Provider, d dictionaries.Provider, hasher hashers.Hasher
 		for {
 			result := <-resultChan
 			for chanID, hash := range result {
-				pb3.Increment()
+				pbCracked.Increment()
 
 				text := hash.GetHash() + "\t" + hash.GetPlain() + "\n"
 				if _, err = f.WriteString(text); err != nil {
@@ -109,14 +121,18 @@ func Run(h dictionaries.Provider, d dictionaries.Provider, hasher hashers.Hasher
 	pool.Stop()
 }
 
-func worker(id int, wg *sync.WaitGroup, errChan chan error, dictionary []string, resultChan chan map[int]Hash) (chan Hash, chan Hash) {
-	logrus.WithField("worker", id).WithField("words", dictionary).Debug("Dictionary")
+func worker(id int, wg *sync.WaitGroup, errChan chan error, dictionary []string, resultChan chan map[int]Hash) (chan Hash, chan Hash, *pb.ProgressBar) {
+	logrus.WithField("Worker", id).WithField("words", dictionary).Debug("Dictionary")
 	resetChan := make(chan Hash)
 	hashesChan := make(chan Hash)
+	bar := pb.New(len(dictionary)).SetUnits(pb.U_NO).Prefix(fmt.Sprintf("worker %d", id))
+	bar.ShowPercent = true
+	bar.ShowTimeLeft = false
 
 	go func() {
 		defer wg.Done()
 		for hash := range hashesChan {
+			bar.Set(0)
 		start:
 			for _, plain := range dictionary {
 				select {
@@ -138,11 +154,12 @@ func worker(id int, wg *sync.WaitGroup, errChan chan error, dictionary []string,
 						break start
 					}
 				}
+				bar.Increment()
 			}
 		}
 	}()
 
-	return resetChan, hashesChan
+	return resetChan, hashesChan, bar
 }
 
 func closeWorkers(hashesChans map[int]chan Hash) {
