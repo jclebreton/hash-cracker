ef0d0f7f5dcf49e0d2a21738a62ef0d1d8433166
JC Lebreton <jclebreton@gmail.com>
2018-02-24 16:34:03 +0100

    Enanle linter on Travis CI

---
+++ scripts/install_linter.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+go get -u gopkg.in/alecthomas/gometalinter.v2
+gometalinter.v2 --install
--- scripts/linter.sh
+++ scripts/linter.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#gometalinter.v2 --install
 gometalinter.v2 --exclude vendor --deadline=60s -D aligncheck -D gotype -D dupl -D gocyclo -D vetshadow -D ineffassign -D gas -D staticcheck --min-occurrences=7 ./...
