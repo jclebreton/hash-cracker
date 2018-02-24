c26638f591045472e3de7eb4c2f7cce79c26d9d1
JC Lebreton <jclebreton@gmail.com>
2018-02-24 11:35:23 +0100

    Add basic test for Hash domain object

---
+++ domains/hash_test.go
@@ -0,0 +1,19 @@
+package domains
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHash_properties(t *testing.T) {
+	hash := Hash{
+		Hash: "a",
+		Salt: "b",
+		Plain: "c",
+	}
+	assert.Equal(t, "a", hash.Hash)
+	assert.Equal(t, "b", hash.Salt)
+	assert.Equal(t, "c", hash.Plain)
+}
